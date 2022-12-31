44adf9a10b617623b6b5da8c7583ae046a2463e4
Yusuf Fakhreddin <48655310+Yusuf-Fakhreddin@users.noreply.github.com>
2022-12-31 02:49:28 +0200

    Initial commit

---
+++ Singleton/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Singleton
+{
+
+    class Singleton
+    {
+        // static flag for object creation
+        private static bool Flag = false;
+        // private constructor to disallow normal object creation
+        private Singleton()
+        {
+
+        }
+        public static Singleton CreateObj()
+        {
+            // if no object was created, create one
+            if (Flag==false)
+            {
+                Singleton i = new Singleton();
+                Flag = true;
+                return i;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void Speak()
+        {
+            Console.WriteLine("Hello There from Singleton Design Pattern");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Singleton i = Singleton.CreateObj();
+            i.Speak();
+            // Throws an error
+            Singleton i2 = Singleton.CreateObj();
+            i2.Speak();
+
+        }
+    }
+}
